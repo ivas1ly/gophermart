c5d6ed0ec015d1123829116687cb11fb654de7ba
Vasily Yenin <vasily.yenin@gmail.com>
2024-02-04 23:43:59 +0700

    test: add more test cases for lunh algorithm

--- internal/utils/lunh/lunh_test.go
+++ internal/utils/lunh/lunh_test.go
@@ -12,6 +12,26 @@ func TestLunh(t *testing.T) {
 		assert.Equal(t, ok, true)
 	})
 
+	t.Run("correct number 2", func(t *testing.T) {
+		ok := CheckNumber("41632078327500")
+		assert.Equal(t, ok, true)
+	})
+
+	t.Run("correct number 3", func(t *testing.T) {
+		ok := CheckNumber("45444541846")
+		assert.Equal(t, ok, true)
+	})
+
+	t.Run("correct number 4", func(t *testing.T) {
+		ok := CheckNumber("333207722682")
+		assert.Equal(t, ok, true)
+	})
+
+	t.Run("correct number 5", func(t *testing.T) {
+		ok := CheckNumber("836361")
+		assert.Equal(t, ok, true)
+	})
+
 	t.Run("incorrect number", func(t *testing.T) {
 		ok := CheckNumber("2377225621")
 		assert.Equal(t, ok, false)
